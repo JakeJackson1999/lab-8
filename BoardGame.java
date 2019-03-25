802e63e6c27c56d7d8da373c841ba9bf17812b9c
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-25 17:57:55 -0500

    First commit for all classes

---
+++ BoardGame.java
@@ -0,0 +1,52 @@
+
+public class BoardGame {
+	
+	public BoardGame() {
+		
+	}
+	
+	public boolean addPlayer(String playerName, GamePiece gamePiece, Location initialLocation) {
+		
+	}
+	
+	 public GamePiece getPlayerGamePiece(String playerName) {
+		 
+	 }
+	
+	 public String getPlayerWithGamePiece(GamePiece gamePiece) {
+		 
+	 }
+	 
+	 public void movePlayer(String playerName, Location newLocation) {
+		 
+	 }
+	 
+	 public String[] moveTwoPlayers(String[] playerNames, Location[] newLocations) {
+		 
+	 }
+	 
+	 public Location getPlayersLocation(String player) {
+		 
+	 }
+	 
+	 public ArrayList<String> getPlayersAtLocation(Location loc){
+		 
+	 }
+	 
+	 public ArrayList<GamePiece> getGamePiecesAtLocation(Location loc){
+		 
+	 }
+	 
+	 public Set<String> getPlayers(){
+		 
+	 }
+	 
+	 public Set<Location> getPlayerLocations(){
+		 
+	 }
+	 
+	 public Set<GamePiece> getPlayerPieces(){
+		 
+	 }
+	 
+}
---
+++ GamePieceAppearance.java
@@ -0,0 +1,24 @@
+
+public class GamePieceAppearance {
+	
+	private Color color() {
+		
+	}
+	
+	private Shape shape() {
+		
+	}
+	
+	public GamePieceAppearance(Color color, Shape shape) {
+		
+	}
+	
+	public Color getColor() {
+		
+	}
+	
+	public Shape getShape() {
+		
+	}
+
+}
