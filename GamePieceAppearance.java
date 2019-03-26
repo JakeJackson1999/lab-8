0a709482606c2e7c6970e3248773499c3eaad45f
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-26 10:59:48 -0500

    finished GamePieceAppearance

--- GamePieceAppearance.java
+++ GamePieceAppearance.java
@@ -1,24 +1,21 @@
 
 public class GamePieceAppearance {
 	
-	private Color color() {
-		
-	}
+	private Color color;
 	
-	private Shape shape() {
-		
-	}
+	private Shape shape;
 	
 	public GamePieceAppearance(Color color, Shape shape) {
-		
+		this.color = color;
+		this.shape = shape;
 	}
 	
 	public Color getColor() {
-		
+		return color;
 	}
 	
 	public Shape getShape() {
-		
+		return shape;
 	}
 
 }
